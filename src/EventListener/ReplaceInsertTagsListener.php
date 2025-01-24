6a0908a2811bac32c0eb7bde2487d3a07e6b8983
Philipp Seibt <seibt@pdir.de>
2025-01-24 16:38:37 +0100

    Fix redundant title text for font size switcher

--- src/EventListener/ReplaceInsertTagsListener.php
+++ src/EventListener/ReplaceInsertTagsListener.php
@@ -32,7 +32,7 @@ class ReplaceInsertTagsListener
     private const AssetsPath = 'bundles/themecomponents';
 
     private const SVG_CIRCLE_TEMPLATE = "<svg xmlns='http://www.w3.org/2000/svg' height='48' width='48' class='%s' onclick='%s'><title>%s</title><desc>%s</desc><path fill='currentColor' d='M24 44q-4.15 0-7.8-1.575-3.65-1.575-6.35-4.275-2.7-2.7-4.275-6.35Q4 28.15 4 24t1.575-7.8Q7.15 12.55 9.85 9.85q2.7-2.7 6.35-4.275Q19.85 4 24 4t7.8 1.575q3.65 1.575 6.35 4.275 2.7 2.7 4.275 6.35Q44 19.85 44 24t-1.575 7.8q-1.575 3.65-4.275 6.35-2.7 2.7-6.35 4.275Q28.15 44 24 44Zm1-3q6.85-.5 11.425-5.3Q41 30.9 41 24q0-6.9-4.575-11.7Q31.85 7.5 25 7Z'/></svg>";
-    private const SVG_SIZE_TEMPLATE = "<svg xmlns='http://www.w3.org/2000/svg' height='48' width='48' class='%s'><title>%s</title><path fill='currentColor' d='M28.5 40V13H18V8h26v5H33.5v27Zm-18 0V23H4v-5h18v5h-6.5v17Z'/></svg>";
+    private const SVG_SIZE_TEMPLATE = "<svg xmlns='http://www.w3.org/2000/svg' height='48' width='48' class='%s'><title>%s</title><desc>%s</desc><path fill='currentColor' d='M28.5 40V13H18V8h26v5H33.5v27Zm-18 0V23H4v-5h18v5h-6.5v17Z'/></svg>";
     /**
      * @var string contao char(1) = '1' flag to allow to switch in high contrast mode
      */
@@ -148,6 +148,7 @@ class ReplaceInsertTagsListener
         $svgSizeIcon = sprintf(
             self::SVG_SIZE_TEMPLATE,
             'wcag-size', // css class
+            $alt,
             $title
         );
 
